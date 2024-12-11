12f38102238a88c5e9647550486c3ff97cc40919
rahmanchik <nahorniyroman@gmail.com>
2024-12-11 19:32:25 +0200

    bb

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event(); window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+    sf::CircleShape shape(10.f);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
-
-        window.clear();
+        window.clear(sf::Color::Black);
+        shape.setFillColor(sf::Color::Green);
+        window.draw(shape);
         window.display();
-    }
+    }   
+    return 0;
 }
